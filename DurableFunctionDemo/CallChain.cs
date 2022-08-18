5a045c57782bd4500a4626e3dc41ff68dd44fc89
Anton Pavlov <anton_pavlov1@epam.com>
2022-08-18 19:24:16 +0200

    Chain Function

---
+++ DurableFunctionDemo/CallChain.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+
+namespace DurableFunctionDemo
+{
+    public static class CallChain
+    {
+        [FunctionName("CallChain")]
+        public static async Task<string> CallChainOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context,
+            ILogger log)
+        {
+            log.LogWarning("Before {functionName}", nameof(Link1));
+            var link1 = await context.CallActivityAsync<int>(nameof(Link1), context.GetInput<string>());
+            log.LogWarning("Before {functionName}", nameof(Link2));
+            var link2 = await context.CallActivityAsync<string>(nameof(Link2), link1);
+            log.LogWarning("Before {functionName}", nameof(Link3));
+            var result = await context.CallActivityAsync<string>(nameof(Link3), link2);
+
+            return result;
+        }
+
+        [FunctionName(nameof(Link1))]
+        public static async Task<int> Link1([ActivityTrigger] string input, ILogger log)
+        {
+            log.LogInformation("In {functionName}; {data}", nameof(Link1), input);
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            return int.Parse(input);
+        }
+
+        [FunctionName(nameof(Link2))]
+        public static async Task<string> Link2([ActivityTrigger] int toFormat, ILogger log)
+        {
+            log.LogInformation("In {functionName}, {data}", nameof(Link2), toFormat);
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            return $"Formatted data {toFormat}";
+        }
+
+        [FunctionName(nameof(Link3))]
+        public static async Task<string> Link3([ActivityTrigger] string toWrap, ILogger log)
+        {
+            log.LogInformation("In {functionName}, {data}", nameof(Link3), toWrap);
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            return $"Wrapped data [{toWrap}]";
+        }
+    }
+}
--- DurableFunctionDemo/OrchestratorExamples.cs
+++ DurableFunctionDemo/OrchestratorExamples.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
@@ -10,38 +9,19 @@ namespace DurableFunctionDemo
 {
     public static class OrchestratorExamples
     {
-        [FunctionName("OrchestratorExamples")]
-        public static async Task<List<string>> RunOrchestrator(
-            [OrchestrationTrigger] IDurableOrchestrationContext context)
-        {
-            var outputs = new List<string>();
-
-            // Replace "hello" with the name of your Durable Activity Function.
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "Tokyo"));
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "Seattle"));
-            outputs.Add(await context.CallActivityAsync<string>("Function1_Hello", "London"));
-
-            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
-            return outputs;
-        }
-
-        [FunctionName("Function1_Hello")]
-        public static string SayHello([ActivityTrigger] string name, ILogger log)
-        {
-            log.LogInformation($"Saying hello to {name}.");
-            return $"Hello {name}!";
-        }
-
-        [FunctionName("Function1_HttpStart")]
+        [FunctionName(nameof(HttpStart))]
         public static async Task<HttpResponseMessage> HttpStart(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
             [DurableClient] IDurableOrchestrationClient starter,
             ILogger log)
         {
-            // Function input comes from the request content.
-            string instanceId = await starter.StartNewAsync("OrchestratorExamples", null);
+            var sampleName = req.RequestUri.ParseQueryString()["name"];
+            var sampleData = req.RequestUri.ParseQueryString()["data"];
+            log.LogInformation($"Starting orchestration '{sampleName}'.");
+            
+            string instanceId = await starter.StartNewAsync(sampleName, null, sampleData);
 
-            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+            log.LogInformation($"Started orchestration '{sampleName}' with ID = '{instanceId}'.");
 
             return starter.CreateCheckStatusResponse(req, instanceId);
         }
