1e8542bb7ab1ffae8c3440037d2d7cfa3f0d8592
Anton Pavlov <anton_pavlov1@epam.com>
2022-09-08 21:05:34 +0200

    Final saga

--- Saga/SagaOrchestrator/SagaFunction.cs
+++ Saga/SagaOrchestrator/SagaFunction.cs
@@ -2,6 +2,8 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -25,19 +27,47 @@ namespace SagaOrchestrator
         {
             var seats = new[] { 5, 7 };
             var customerData = "test customer";
-            var seatsStatus = await context.CallActivityAsync<SeatsStatus>("BookSeats", seats);
-            var paymentStatus = await context.CallSubOrchestratorAsync<PaymentStatus>("Pay", seatsStatus.Price);
-            var registrationStatus = await context.CallActivityAsync<RegistrationStatus>("RegisterAndNotify", new RegistrationData(seats, paymentStatus.TransactionId, customerData));
 
-            return new BookingStatus(true, registrationStatus.OrderId);
+            var rollBackCollection = new Stack<Func<Task>>();
+            do
+            {
+                var seatsStatus = await context.CallActivityAsync<SeatsStatus>("BookSeats", seats);
+                if (!seatsStatus.Succeed) break;
+                rollBackCollection.Push(async () => await context.CallActivityAsync("ReleaseSeats", seats));
+
+                var paymentStatus = await context.CallSubOrchestratorAsync<PaymentStatus>("Pay", "eventinstance", seatsStatus.Price);
+                if(!paymentStatus.Succeed) break;
+                rollBackCollection.Push(async () => await context.CallActivityAsync("CancelPayment", paymentStatus.TransactionId));
+
+                var registrationStatus = await context.CallActivityAsync<RegistrationStatus>("RegisterAndNotify",
+                    new RegistrationData(seats, paymentStatus.TransactionId, customerData));
+                
+                if (!registrationStatus.Succeed) break;
+                
+                return new BookingStatus(true, registrationStatus.OrderId);
+            } while (false);
+
+            while (rollBackCollection.TryPop(out var rollBack))
+            {
+                await rollBack();
+            }
+
+            return new BookingStatus(false, null);
         }
 
         [FunctionName("BookSeats")]
         public static SeatsStatus BookSeats([ActivityTrigger] int[] seats, ILogger log)
         {
-            log.LogInformation($"Booking seats {string.Join(", ", seats.ToString())}.");
+            log.LogWarning($"Booking seats '{string.Join(", ", seats.Select(seat => seat.ToString()))}'.");
             return new SeatsStatus(true, 1000);
         }
+
+        [FunctionName("ReleaseSeats")]
+        public static void ReleaseSeats([ActivityTrigger] int[] seats, ILogger log)
+        {
+            log.LogWarning($"Releasing seats '{string.Join(", ", seats.Select(seat => seat.ToString()))}'.");
+        }
+
 
         [FunctionName("Pay")]
         public static async Task<PaymentStatus> Pay([OrchestrationTrigger] IDurableOrchestrationContext context, ILogger log)
@@ -45,7 +75,7 @@ namespace SagaOrchestrator
             const string eventName = "payEvent";
             var price = context.GetInput<decimal>();
             var result = false;
-            log.LogInformation($"Paying seats {price}$. Raise event '{eventName}'");
+            log.LogWarning($"Paying seats {price}$. Raise event '{eventName}'");
 
             using var timeoutCts = new CancellationTokenSource();
 
@@ -71,13 +101,20 @@ namespace SagaOrchestrator
                 timeoutCts.Cancel(); // All pending timers must be complete or canceled before the function exits.
             }
 
-            return new PaymentStatus(result, result ? new Guid(): Guid.Empty);
+            return new PaymentStatus(result, result ? Guid.NewGuid() : Guid.Empty);
         }
+
+        [FunctionName("CancelPayment")]
+        public static void CancelPayment([ActivityTrigger] Guid paymentId, ILogger log)
+        {
+            log.LogWarning($"Cancel payment '{paymentId}'.");
+        }
+
 
         [FunctionName("RegisterAndNotify")]
         public static RegistrationStatus RegisterAndNotify([ActivityTrigger] RegistrationData registrationData, ILogger log)
         {
-            log.LogInformation($"Booking seats {registrationData}.");
+            log.LogWarning($"Register And Notify {registrationData}.");
             return new RegistrationStatus(true, "2022 0009 1234");
         }
     }
